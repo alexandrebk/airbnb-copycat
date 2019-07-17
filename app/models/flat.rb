3fc8a5d066d05e157dec5fbe204c78a083ad659c
Philippine Berton <Phi.berton@hotmail.fr>
2019-07-17 15:08:06 +0200

    add map

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,6 +2,12 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many   :bookings, dependent: :destroy
 
+  validates :address, presence: true
+  validates :surface, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :room, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
+  validates :max_guests, presence: true, numericality: { only_integer: true }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
