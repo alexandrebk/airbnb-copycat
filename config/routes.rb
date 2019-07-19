a0c1ec61c8569fed2f2d5dcb198be26610801ab8
Philippine Berton <Phi.berton@hotmail.fr>
2019-07-19 09:25:32 +0200

    add controller bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :flats do
-    resources :bookings, only: [:create, :new]
+    resources :bookings, only: [:create, :new, :edit, :update]
   end
   get 'dashboard', to: 'pages#dashboard'
+  resources :bookings, only: [:index, :show, :destroy]
 
 end
