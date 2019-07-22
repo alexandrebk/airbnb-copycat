ad364d9c9f41ec24a57e5428f1b5ce58e56a3b2d
Philippine Berton <Phi.berton@hotmail.fr>
2019-07-22 22:42:35 +0200

    administrate

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,9 +1,5 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
-
-    def authenticate_admin
-      http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
-    end
+    http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
   end
 end
