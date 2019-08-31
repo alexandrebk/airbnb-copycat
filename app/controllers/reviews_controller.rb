1ec0bd242e20d2d8e5fe71be518575dffb00667d
Alexandre Bouvier <alexandre.b2506@gmail.com>
2019-08-31 16:05:49 +0200

    wip

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,16 +4,14 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @user        = current_user
-    @flat        = Flat.find(params[:flat_id])
-    @booking     = Booking.find(params[:id])
-    @review      = Review.new(review_params)
-    @review.user = current_user
-    @review.flat = @flat
+    @review         = Review.new(review_params)
+    @booking        = Booking.find(params[:booking_id])
     @review.booking = @booking
+    @review.user    = current_user
     if @review.save
       redirect_to flat_path(@flat.id)
     else
+      @flat = @review.booking.flat
       render "bookings/show"
     end
   end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -19,4 +19,7 @@ class Flat < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
+
+  def average
+  end
 end
