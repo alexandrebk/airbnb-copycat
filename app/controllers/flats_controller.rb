98f251daf543aac5b20b16aa68bdc05295c91573
Philippine Berton <Phi.berton@hotmail.fr>
2020-04-06 10:27:25 +0200

    Synthax flat_controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,7 +7,7 @@ class FlatsController < ApplicationController
     @flats = Flat.where.not(latitude: nil, longitude: nil)
     if flats_filter.present?
       if flats_filter[:search].present?
-        @flats = Flat.search_by_description_and_address("%#{flats_filter[:search]}%")
+        @flats = Flat.search_by_description_and_address(flats_filter[:search])
       end
       if flats_filter[:min_price].present?
         @flats = @flats.where("flats.price >= ?", flats_filter[:min_price])
